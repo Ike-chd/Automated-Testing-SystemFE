d57a94b2aab5427f26000300a02510600246d788
1ucy666 <94223862+1ucy666@users.noreply.github.com>
2023-11-22 14:53:26 +0200

    nsnsns

--- src/main/java/Services/UserHandler.java
+++ src/main/java/Services/UserHandler.java
@@ -32,29 +32,29 @@ public class UserHandler {
     }
 
     // Add additional methods as needed for user management
-
-    public static void main(String[] args) {
-        UserHandler userHandler = new UserHandler();
-
-        // Example: Adding students (celebrities)
-        Student celebrity1 = new Student(1, "Tom", "Hanks", "tom@example.com", "111111", "123 Main St", 1, "tomspassword");
-        Student celebrity2 = new Student(2, "Angelina", "Jolie", "angelina@example.com", "222222", "456 Oak St", 2, "angelinapassword");
-
-        userHandler.addUser(celebrity1);
-        userHandler.addUser(celebrity2);
-
-        // Example: Retrieving and updating user information
-        Optional<UserInterface> foundUser = userHandler.getUserByID(1);
-        foundUser.ifPresent(user -> {
-            System.out.println("Found student: " + user.getFirstname() + " " + user.getSurname());
-            userHandler.updateUserPassword(user.getUserID(), "newStudentPassword");
-        });
-
-        // Example: Displaying all users
-        List<UserInterface> allUsers = userHandler.getAllUsers();
-        System.out.println("All Users:");
-        for (UserInterface user : allUsers) {
-            System.out.println(user.getFirstname() + " " + user.getSurname() + " - " + user.getEmail());
-        }
-    }
+//
+//    public static void main(String[] args) {
+//        UserHandler userHandler = new UserHandler();
+//
+//        // Example: Adding students (celebrities)
+//        Student celebrity1 = new Student(1, "Tom", "Hanks", "tom@example.com", "111111", "123 Main St", 1, "tomspassword");
+//        Student celebrity2 = new Student(2, "Angelina", "Jolie", "angelina@example.com", "222222", "456 Oak St", 2, "angelinapassword");
+//
+//        userHandler.addUser(celebrity1);
+//        userHandler.addUser(celebrity2);
+//
+////        // Example: Retrieving and updating user information
+////        Optional<UserInterface> foundUser = userHandler.getUserByID(1);
+////        foundUser.ifPresent(user -> {
+////            System.out.println("Found student: " + user.getFirstname() + " " + user.getSurname());
+////            userHandler.updateUserPassword(user.getUserID(), "newStudentPassword");
+////        });
+//
+//        // Example: Displaying all users
+//        List<UserInterface> allUsers = userHandler.getAllUsers();
+//        System.out.println("All Users:");
+//        for (UserInterface user : allUsers) {
+//            System.out.println(user.getFirstname() + " " + user.getSurname() + " - " + user.getEmail());
+//        }
+//    }
 }
