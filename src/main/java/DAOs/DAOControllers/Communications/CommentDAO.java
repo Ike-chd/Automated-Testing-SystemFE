61376f4f87c902287bb8239a379c2fc53b813f27
Ike1904 <63149462+Ike1904@users.noreply.github.com>
2023-11-22 08:40:13 +0200

    Comments

--- src/main/java/DAOs/DAOControllers/Communications/CommentDAO.java
+++ src/main/java/DAOs/DAOControllers/Communications/CommentDAO.java
@@ -1,10 +1,15 @@
 package DAOs.DAOControllers.Communications;
 
 import Models.Communication.Comment;
+import Models.Users.FacultyMember;
+import Models.Users.Student;
+import java.util.List;
 
 public interface CommentDAO {
     public Comment getComment();
     public boolean insertComment(Comment commnet);
     public boolean updateComment(Comment comment);
     public boolean deleteComment(Comment comment);
+    public List<Comment> getAllStudentComments(Student student);
+    public List<Comment> getAllCommentsByFaculty(FacultyMember faculty);
 }
--- src/main/java/Models/Report/PerformanceReport.java
+++ src/main/java/Models/Report/PerformanceReport.java
@@ -1,17 +1,18 @@
 package Models.Report;
 
+import Models.Courses.Topic;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 @AllArgsConstructor
 @Getter
 @Setter
 @NoArgsConstructor
 public class PerformanceReport extends Report {
-
-    private List<String> hardestTopics;
+    Topic[] hardestTopics = new Topic[5];
     private List<String> hardestTests;
     private List<String> averageStudentPerformance;
     private List<String> individualStudentPerformance;
