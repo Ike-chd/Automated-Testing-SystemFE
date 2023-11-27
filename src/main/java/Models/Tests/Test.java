983dd832be3711277f197ca8ca3e77673b5273ef
Ridwaan <134335143+Ridwaan1612@users.noreply.github.com>
2023-11-27 08:52:49 +0200

    Merge origin/ike into ridwaan
    Conflicts:
    	pom.xml
    	src/main/java/com/mycompany/atsbe/JakartaRestConfiguration.java
    	src/main/java/com/mycompany/atsbe/resources/JakartaEE91Resource.java

--- src/main/java/Models/Tests/Test.java
+++ src/main/java/Models/Tests/Test.java
@@ -2,11 +2,15 @@ package Models.Tests;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Setter
 
 public class Test {
 
