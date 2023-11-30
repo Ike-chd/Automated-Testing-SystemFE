1c007b3f5d962053e533c490792f0d3c82cbd46c
JnrMpofu <tjmpofu2@gmail.com>
2023-11-30 11:06:44 +0200

    Merge origin/ike into junior
    Conflicts:
    	src/main/java/DAOs/DAOControllers/QA/QuestionDAO.java
    	src/main/java/Models/QA/Question.java
    	src/main/java/Services/CourseHandler.java
    	src/main/java/Services/QuestionHandler.java
    	src/main/java/Services/ServicesInterfaces/TopicService.java
    	src/main/java/com/mycompany/atsbe/resources/CourseREST.java
    	src/main/java/com/mycompany/atsbe/resources/QuestionREST.java
    	src/main/java/com/mycompany/atsbe/resources/TestREST.java
    	src/main/java/com/mycompany/atsbe/resources/TopicREST.java

--- src/main/java/com/mycompany/atsfe/resources/UserHandler.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.atsfe.resources;
-
-public class UserHandler{
-    UserDB udb = new UserDB();
-    
-    public void addAccountRequest(User student) {
-        udb.createAccountReq(student);
-    }
-}
--- src/main/webapp/JavaScripts/index.js
+++ src/main/webapp/JavaScripts/index.js
@@ -10,20 +10,24 @@ $(function () {
     var $id = $('#idnumber');
 
     $('#submit').click(function () {
+        var account = {
+            name: $name.val(),
+            surname: $surname.val(),
+            email: $email.val(),
+            idNumber: $id.val()
+        };
+        
         var settings = {
             url: "http://localhost:8080/Automated-Testing-SystemBE/resources/account/request",
             method: "POST",
             timeout: 0,
-            headers : {
+            headers: {
                 "Content-Type": "application/json"
             },
-            data: JSON.stringify({
-                name: $name.val(),
-                surname: $surname.val(),
-                email: $email.val(),
-                idNumber: $id.val()
-            })
+            data: JSON.stringify(account)
         };
+
+        console.log(account);
 
         $.ajax(settings).done(function (response) {
             console.log(response);
