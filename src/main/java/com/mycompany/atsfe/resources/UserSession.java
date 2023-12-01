856a1c5d62479506f003a1e5929bc8a2b15d7860
Ike1904 <63149462+Ike1904@users.noreply.github.com>
2023-12-01 08:48:31 +0200

    Merge origin/junior into ike
    Conflicts:
    	src/main/java/DBConnection/DBConnection.java

---
+++ src/main/java/com/mycompany/atsfe/resources/UserSession.java
@@ -0,0 +1,26 @@
+package com.mycompany.atsfe.resources;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "UserSession", urlPatterns = {"/UserSession"})
+public class UserSession extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        session.setAttribute("user", this);
+    }
+}
