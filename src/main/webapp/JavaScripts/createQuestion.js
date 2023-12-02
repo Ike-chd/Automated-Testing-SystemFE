162532668268fce1b17849c9b564a484e6058f0e
Ike1904 <63149462+Ike1904@users.noreply.github.com>
2023-12-02 08:38:40 +0200

    Merge origin/junior into ike
    Conflicts:
    	src/main/java/DBConnection/DBConnection.java

---
+++ src/main/webapp/JavaScripts/createQuestion.js
@@ -0,0 +1,24 @@
+$(function(){
+    var $qname = $('#qname');
+    var $mark = $('#mark');
+    var $q1 = $('#q1');
+    var $q2 = $('#q2');
+    var $q3 = $('#q3');
+    var $q4 = $('#q4');
+    var ans = 1;
+    
+    $('#submit').click(function() {
+        var data = {
+            question : $qname.val(),
+            mark : $mark.val(),
+            q1 : $q1.val()
+        };
+        
+        console.log(data);
+    });
+    $('#add').click(function () {
+        ans++;
+        $('#inputs').append('<input type="text" id="q'+ans+'">');
+    });
+    
+});
