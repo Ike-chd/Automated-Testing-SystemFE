cf25504ea0bc9409e3282a43c6a8c88e3afae546
1ucy666 <94223862+1ucy666@users.noreply.github.com>
2023-12-05 11:44:21 +0200

    Merge origin/ridwaan into 1ucy666
    Conflicts:
    	pom.xml
    	src/main/webapp/index.html

--- src/main/webapp/JavaScripts/createQuestion.js
+++ src/main/webapp/JavaScripts/createQuestion.js
@@ -1,22 +1,10 @@
-$(function(){
-    var $qname = $('#qname');
-    var $mark = $('#mark');
-    var $q1 = $('#q1');
-    var $q2 = $('#q2');
-    var $q3 = $('#q3');
-    var $q4 = $('#q4');
+
+
+(function () {
     var ans = 1;
-    
-    $('#submit').click(function() {
-        var data = {
-            question : $qname.val(),
-            mark : $mark.val(),
-            q1 : $q1.val()
-        };
-        console.log(data);
-    });
+
     $('#add').click(function () {
         ans++;
-        $('#answers').append('<input type="text" id="q'+ans+'">');
+        $('#answers').append('<input type="text" id="q' + ans + '">');
     });
-});
+})();
