6e5a2e8dc53b18d13c58f0d0ed738d937fb2b3c5
Ike1904 <63149462+Ike1904@users.noreply.github.com>
2023-12-05 11:42:29 +0200

    Create Question html, js and css

--- src/main/webapp/JavaScripts/createQuestion.js
+++ src/main/webapp/JavaScripts/createQuestion.js
@@ -1,22 +1,31 @@
-$(function(){
-    var $qname = $('#qname');
+$(function () {
+    var $qname = $('#q');
     var $mark = $('#mark');
-    var $q1 = $('#q1');
-    var $q2 = $('#q2');
-    var $q3 = $('#q3');
-    var $q4 = $('#q4');
-    var ans = 1;
-    
-    $('#submit').click(function() {
+    var ans = 0;
+    var check = 0;
+
+    $('#submit').click(function () {
+        var answers = $('.answers').map(function () {
+            check++;
+            return {
+                answer: this.value,
+                isCorrect: document.querySelector('#check' + check).checked
+            };
+        }).get();
         var data = {
-            question : $qname.val(),
-            mark : $mark.val(),
-            q1 : $q1.val()
+            question: $qname.html(),
+            mark: $mark.val(),
+            answers: answers
         };
         console.log(data);
     });
     $('#add').click(function () {
         ans++;
-        $('#answers').append('<input type="text" id="q'+ans+'">');
+        $('#answers').append('\
+<input type="text" class="answers" id="q' + ans + '">\n\
+<label class="container">\n\
+    <input type="checkbox" id="check'+ ans +'">\n\
+    <span class="checkmark"></span>\n\
+</label>');
     });
 });
