e1ca2e4ef5cb042c759ef462e164271c77e08ed0
Ike1904 <63149462+Ike1904@users.noreply.github.com>
2023-12-06 10:25:10 +0200

    create account

--- src/main/webapp/JavaScripts/createAccount.js
+++ src/main/webapp/JavaScripts/createAccount.js
@@ -1,34 +1,94 @@
-$(document).ready(function () {
-    $('#createAccountForm').submit(function (event) {
-        event.preventDefault();
-
-        var username = $('#username').val();
-        var email = $('#email').val();
-        var password = $('#password').val();
-        var role = $('#role').val();
-
-        var accountData = {
-            username: username,
-            email: email,
-            password: password,
-            role: role
+$(function () {
+    var $sbtn = $('#sbtn');
+    var $fbtn = $('#fbtn');
+    var $abtn = $('#abtn');
+    $sbtn.click(function () {
+        $('#wr2').fadeOut(1000);
+        $('#wr3').fadeOut(1000);
+        $('#wr1').fadeIn(1000);
+    });
+    $fbtn.click(function () {
+        $('#wr1').fadeOut(1000);
+        $('#wr3').fadeOut(1000);
+        $('#wr2').fadeIn(1000);
+    });
+    $abtn.click(function () {
+        $('#wr1').fadeOut(1000);
+        $('#wr2').fadeOut(1000);
+        $('#wr3').fadeIn(1000);
+    });
+    
+    $('#createS').click(function () {
+        var course = {
+            name: $('#name').val(),
+            surname: $('#surname').val(),
+            email: $('#email').val(),
+            idNumber: $('#id').val(),
+            address: $('#address').val(),
+            phoneNumber: $('#phone').val()
+        };
+        var settings = {
+            url: "http://192.168.80.104:8080/Automated-Testing-SystemBE/resources/students/createAccount/1",
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(course)
         };
 
-        var apiUrl = 'http://your-api-endpoint/createAccount';
+        console.log(course);
 
-        $.ajax({
-            url: apiUrl,
-            method: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(accountData),
-            success: function (response) {
-                console.log('Account created successfully:', response);
-                alert('Account created successfully!');
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+    });
+    
+    $('#createF').click(function () {
+        var course = {
+            name: $('#name').val(),
+            surname: $('#surname').val(),
+            email: $('#email').val(),
+            idNumber: $('#id').val()
+        };
+        var settings = {
+            url: "http://192.168.80.104:8080/Automated-Testing-SystemBE/resources/accounts/createAccount",
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
             },
-            error: function (error) {
-                console.error('Error creating account:', error);
-                alert('Error creating account. Please try again.');
-            }
+            data: JSON.stringify(course)
+        };
+
+        console.log(course);
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+    });
+    
+    $('#createA').click(function () {
+        var data = {
+            name: $('#name3').val(),
+            surname: $('#surname3').val(),
+            email: $('#email3').val(),
+            idNumber: $('#id3').val()
+        };
+        var settings = {
+            url: "http://192.168.80.104:8080/Automated-Testing-SystemBE/resources/courses/create",
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(data)
+        };
+
+        console.log(data);
+
+        $.ajax(settings).done(function (response) {
+            console.log(response);
         });
     });
 });
