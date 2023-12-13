8b4b7385a5b07caae43590aa5544511c9968aa83
Ridwaan <134335143+Ridwaan1612@users.noreply.github.com>
2023-12-13 12:31:29 +0200

    Merge origin/ike2 into ridwaan
    Conflicts:
    	src/main/webapp/JavaScripts/createQuestion.js
    	src/main/webapp/JavaScripts/login.js
    	src/main/webapp/createQuestion.html

--- src/main/webapp/JavaScripts/comment.js
+++ src/main/webapp/JavaScripts/comment.js
@@ -1,22 +1,32 @@
 $(document).ready(function () {
+    $.ajax({
+        type: 'GET',
+        url: "http://localhost:8080/Automated-Testing-SystemBE/resources/students/getStudentByid`",
+        success: function (students) {
+            var selectElement = $('#studentId');
+            students.forEach(function (student) {
+                selectElement.append($('<option>', {
+                    value: student.userID,
+                    text: student.firstname + ' ' + student.surname
+                }));
+            });
+        }
+    });
 
     $('#submitComment').click(function () {
         var studentId = $('#studentId').val();
         var commentText = $('#commentText').val();
-        var ip = "localhost";
 
         $.ajax({
             type: 'POST',
-            url: "http://" + ip + ":8080/Automated-Testing-SystemBE/resources/comments/allComments",
-            data: {
+            url: "http://localhost:8080/Automated-Testing-SystemBE/resources/comments/postComment",
+            contentType: 'application/json',
+            data: JSON.stringify({
                 studentId: studentId,
-                commentText: commentText
-            },
+                comment: commentText
+            }),
             success: function (response) {
                 console.log(response);
-            },
-            error: function (error) {
-                console.error(error);
             }
         });
     });
